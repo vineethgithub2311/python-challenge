0162b2f3bd082319a4f9c8f3de39c1a013272a28
Vineeth <vineethkumar.gattu@gmail.com>
2018-12-08 23:58:24 -0600

    added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,43 @@
 # this is the Pybank challenge
+
+import pandas as pd
+import numpy as np
+
+#path for csv file
+file_path = "/Users/vineethkumargattu/Desktop/Data/python-challenge/PyBank/Resources/budget_data.csv"
+
+#import csv file into pandas dataframe
+df = pd.read_csv(file_path)
+#print (df.head())
+#print (df.dtypes)
+
+print('-------------------------------------------')
+
+#The total number of months included in the dataset
+total_months = df.loc [:, 'Date']
+print (f'Total Months: {total_months.count()}')
+
+# The total net amount of Profit/Losses over entire period
+net = df.loc [:, 'Profit/Losses']
+print (f'Total: ${net.sum()}') 
+
+#The average change in "Profit/Losses" between months over the entire period
+df ['change'] = df['Profit/Losses'].diff()
+
+#change_values = np.around(change_values, 2) # round values?
+df = df.dropna(how='any')
+df2 = round(df['change'].mean(),2)
+print (f'Average Change: $ {df2}')
+
+#The greatest increase in profits (date and amount) over the entire period
+
+df2 = df.loc[df['change'].idxmax()]
+dDte = df2['Date']
+change = df2['change']
+print (f'Greatest Increase in Profits: {dDte} ($ {change})')
+
+#The greatest decrease in losses (date and amount) over the entire period
+df2 = df.loc[df['change'].idxmin()]
+dDte = df2['Date']
+change = df2['change']
+print (f'Greatest Decrease in Profits: {dDte} ($ {change})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,23 @@
 #This is the PyPoll challenge
+
+import pandas as pd
+import numpy as np
+
+#path for csv file
+file_path = "/Users/vineethkumargattu/Desktop/Data/python-challenge/PyPoll/Resources/election_data.csv"
+
+#import csv file into pandas dataframe
+df = pd.read_csv(file_path)
+print (df.head())
+#print (df.dtypes)
+
+print('-------------------------------------------')
+print ('Election Results')
+
+#The total number of votes cast
+total_votes = df.loc [:, 'Voter ID']
+print (f'Total Votes: {total_votes.count()}')
+
+#A complete list of candidates who received votes
+df_candidate_group = df.groupby(['Candidate']).count()
+print(df_candidate_group)
